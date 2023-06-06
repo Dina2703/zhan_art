34bd46b8bc499f1525c581b5c273c62eaeea4057
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-06-06 14:51:15 -0400

    setup env  variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,12 +7,12 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCK8oMcGSVHLJx4IwYQ7ojYNFqZc8b_1RM",
-  authDomain: "zhan-artwork.firebaseapp.com",
-  projectId: "zhan-artwork",
-  storageBucket: "zhan-artwork.appspot.com",
-  messagingSenderId: "887293335215",
-  appId: "1:887293335215:web:9455bfe8db8becdac9265a",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
