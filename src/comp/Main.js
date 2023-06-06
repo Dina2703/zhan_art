ce676dbc3dfc62d837c2a8b2f341eb370d529329
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-06-06 10:16:14 -0400

    starter

---
+++ src/comp/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <h1>Zhan Art Gallery</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+        error.
+      </p>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/comp/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="nav-section">
+      <h1>Zhan K</h1>
+      <ul className="text-red-500">
+        <li>Work</li>
+        <li>About</li>
+        <li>Blog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
