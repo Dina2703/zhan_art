12751c8d7446a44b480d964c7507e7791d82ab9b
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-08-21 15:52:20 -0400

    About page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function About() {
-  return <div className="min-h-screen">About</div>;
+  return (
+    <div className="min-h-fit ">
+      <div className="flex lg:flex-row flex-col gap-10 items-center text-center lg:text-left my-10">
+        <div className="flex-1">
+          <img src="avatar.jpg" alt="Zhan Khvan" />
+        </div>
+        <div className="flex-1">
+          <h1>Hello Hello</h1>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+              perferendis, voluptatibus exercitationem corrupti aspernatur,
+              facere, modi cupiditate ab fuga pariatur fugiat nemo commodi
+              delectus officiis molestiae quis. Ullam recusandae, animi
+              aspernatur, optio perferendis nobis sit, suscipit fugiat commodi
+              at dolorum ex! Quis eos ab debitis assumenda mollitia. Sunt, odit
+              a?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
