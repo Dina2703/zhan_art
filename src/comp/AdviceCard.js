bdf1c2b125f4155efd75f895dbebba02564852fe
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-09-09 21:56:17 -0400

    updated adviceCard  data

--- src/comp/AdviceCard.js
+++ src/comp/AdviceCard.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import divider from "../img/pattern-divider-desktop.svg";
+import { lifeLaws } from "../laws";
 
 import { GrLinkNext } from "react-icons/gr";
 
@@ -7,15 +8,19 @@ function AdviceCard() {
   const [advice, setAdvice] = useState({});
   const [clicked, setClicked] = useState(false);
   const [error, setError] = useState();
+  const [selectedItem, setSelectedItem] = useState("");
 
-  console.log(advice);
+  // console.log(advice);
   useEffect(() => {
-    fetch("https://api.adviceslip.com/advice")
-      .then((res) => res.json())
-      .then((data) => setAdvice(data.slip))
-      .catch((error) => setError(error));
+    // fetch("https://api.adviceslip.com/advice")
+    //   .then((res) => res.json())
+    //   .then((data) => setAdvice(data.slip))
+    //   .catch((error) => setError(error));
+    let randomIndex = Math.floor(Math.random() * lifeLaws.length);
+    setSelectedItem(lifeLaws[randomIndex]);
   }, [clicked]);
 
+  console.log(lifeLaws);
   return (
     <div className="w-full h-auto md:w-72 lg:w-96 lg:h-52 md:h-40 text-center  rounded-lg  bg-gray-500 p-5 flex flex-col gap-3 justify-center  relative shadow-md shadow-slate-700">
       <h3 className="text-cyan-400   text-xs md:text-base  tracking-[2px] capitalize font-semibold">
@@ -24,7 +29,9 @@ function AdviceCard() {
       <div className="h-full flex items-center justify-center">
         <div>
           <p className="text-[#e2e8f0]  md:text-xs text-[16px] pb-4 tracking-wide font-light ">
-            {error ? "loading..." : advice.advice}
+            {/* {error ? "loading..." : advice.advice} */}
+            {/* <span className="font-semibold block">{selectedItem.law}</span> */}
+            <span>{selectedItem.desc}</span>
           </p>
         </div>
       </div>
---
+++ src/laws.js
@@ -0,0 +1,169 @@
+export const lifeLaws = [
+  {
+    law: "Education First",
+    desc: "Prioritize your studies and make the most of your educational opportunities.",
+  },
+  { law: "Work Ethic", desc: "Hard work and dedication are keys to success." },
+  {
+    law: "Continuous Learning",
+    desc: "Never stop learning and seeking knowledge.",
+  },
+  {
+    law: "Resilience",
+    desc: "Learn to bounce back from setbacks and failures.",
+  },
+  {
+    law: "Embrace Change",
+    desc: "Change is inevitable; adapt and grow with it.",
+  },
+  { law: "Health Matters", desc: "Prioritize physical and mental health." },
+  {
+    law: "Financial Responsibility",
+    desc: "Save, invest, and manage money wisely.",
+  },
+  { law: "Kindness", desc: "Treat others with respect and empathy." },
+  { law: "Gratitude", desc: "Be thankful for what you have." },
+  {
+    law: "Time Management",
+    desc: "Efficiently allocate your time and prioritize tasks.",
+  },
+  {
+    law: "Communication Skills",
+    desc: "Learn to express yourself effectively.",
+  },
+  { law: "Confidence", desc: "Believe in yourself and your abilities." },
+  { law: "Empower Others", desc: "Lift others up and support their growth." },
+  {
+    law: "Responsibility",
+    desc: "Take ownership of your actions and decisions.",
+  },
+  { law: "Forgiveness", desc: "Let go of grudges and learn to forgive." },
+  {
+    law: "Goal Setting",
+    desc: "Set clear, achievable goals and work toward them.",
+  },
+  { law: "Adaptability", desc: "Be flexible in different situations." },
+  { law: "Empathy", desc: "Understand and share the feelings of others." },
+  { law: "Mindfulness", desc: "Practice being present in the moment." },
+  {
+    law: "Respect Diversity",
+    desc: "Embrace and respect differences in culture, religion, and ethnicity.",
+  },
+  { law: "Honesty", desc: "Always be truthful and transparent." },
+  {
+    law: "Self-Care",
+    desc: "Prioritize self-care to maintain mental and emotional well-being.",
+  },
+  {
+    law: "Networking",
+    desc: "Build meaningful relationships with people in your field.",
+  },
+  { law: "Environmental Consciousness", desc: "Take care of the planet." },
+  { law: "Patience", desc: "Good things often take time; practice patience." },
+  {
+    law: "Admit Mistakes",
+    desc: "Learn from your errors and take responsibility.",
+  },
+  { law: "Humility", desc: "Stay humble and open to learning from others." },
+  {
+    law: "Critical Thinking",
+    desc: "Analyze information and make informed decisions.",
+  },
+  {
+    law: "Creativity",
+    desc: "Embrace your creative side and think outside the box.",
+  },
+  {
+    law: "Balance",
+    desc: "Strive for a balanced life that includes work, play, and rest.",
+  },
+
+  {
+    law: "Humor",
+    desc: "Find joy in laughter and don't take life too seriously.",
+  },
+  { law: "Leadership", desc: "Develop leadership skills and lead by example." },
+  { law: "Courage", desc: "Face your fears and take calculated risks." },
+  { law: "Self-Reflection", desc: "Regularly assess your actions and goals." },
+  { law: "Teamwork", desc: "Learn to collaborate effectively with others." },
+  { law: "Punctuality", desc: "Respect other people's time by being on time." },
+  {
+    law: "Self-Compassion",
+    desc: "Be kind to yourself, especially during difficult times.",
+  },
+  {
+    law: "Grit",
+    desc: "Persevere through challenges and stay committed to your goals.",
+  },
+  {
+    law: "Integrity",
+    desc: "Always do what's right, even when it's difficult.",
+  },
+  {
+    law: "Simplicity",
+    desc: "Simplify your life to reduce stress and clutter.",
+  },
+  { law: "Generosity", desc: "Give back to your community and those in need." },
+  {
+    law: "Tolerance",
+    desc: "Accept and respect different viewpoints and beliefs.",
+  },
+  {
+    law: "Organization",
+    desc: "Keep your personal and professional life organized.",
+  },
+  { law: "Curiosity", desc: "Stay curious and explore the world around you." },
+  {
+    law: "Accountability",
+    desc: "Hold yourself accountable for your actions.",
+  },
+  { law: "Mentorship", desc: "Seek out mentors and be a mentor to others." },
+  {
+    law: "Travel",
+    desc: "Explore new places and cultures to broaden your perspective.",
+  },
+  {
+    law: "Conflict Resolution",
+    desc: "Learn how to resolve conflicts peacefully.",
+  },
+  { law: "Grit", desc: "Develop mental toughness and perseverance." },
+  {
+    law: "Mindset",
+    desc: "Cultivate a growth mindset that embraces challenges.",
+  },
+  {
+    law: "Resourcefulness",
+    desc: "Find solutions even when resources are limited.",
+  },
+  { law: "Digital Literacy", desc: "Navigate the digital world responsibly." },
+  {
+    law: "Cultural Awareness",
+    desc: "Appreciate and learn from different cultures.",
+  },
+  { law: "Forgiveness", desc: "Let go of past grievances and move forward." },
+  {
+    law: "Positivity",
+    desc: "Maintain a positive attitude even in tough times.",
+  },
+  {
+    law: "Ethical Decision-Making",
+    desc: "Consider the moral implications of your choices.",
+  },
+
+  {
+    law: "Feedback",
+    desc: "Seek and give constructive feedback to grow",
+  },
+  {
+    law: "Self-Advocacy",
+    desc: "Speak up for your needs and goals.",
+  },
+  {
+    law: "Prioritization",
+    desc: "Identify and focus on what truly matters.",
+  },
+  {
+    law: "Persistence",
+    desc: "Keep pushing forward when faced with obstacles.",
+  },
+];
