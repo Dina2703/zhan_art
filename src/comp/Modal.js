a941447035dda309e9be704dfe7768c626a89975
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-10-05 15:27:01 -0400

    Modal text style for the modaile view

--- src/comp/Modal.js
+++ src/comp/Modal.js
@@ -3,7 +3,10 @@ import { motion } from "framer-motion";
 
 function Modal({ selectedImg, setSelectedImg }) {
   const handleCloseModel = (e) => {
-    if (e.target.tagName.toLowerCase() === "div") {
+    if (
+      e.target.tagName.toLowerCase() === "div" ||
+      e.target.tagName.toLowerCase() === "button"
+    ) {
       setSelectedImg(null);
     }
   };
@@ -23,10 +26,15 @@ function Modal({ selectedImg, setSelectedImg }) {
           stiffness: 260,
           damping: 50,
         }}
-        className="flex flex-col lg:flex-row max-w-5xl  m-auto justify-center items-center h-full "
+        className="flex flex-col lg:flex-row max-w-5xl lg:p-20 m-auto justify-center items-center h-full text-slate-200 "
       >
         <div className=" md:w-[60%]   ">
-          {" "}
+          <button
+            className="float-right pr-4 cursor-pointer block md:hidden"
+            onClick={handleCloseModel}
+          >
+            X
+          </button>{" "}
           <img
             // initial={{ y: "-100vh" }}
             // animate={{ y: 0 }}
@@ -42,7 +50,7 @@ function Modal({ selectedImg, setSelectedImg }) {
           <p className=" text-xs font-serif lowercase ">{selectedImg.type}</p>{" "}
         </div>
 
-        <div className=" w-full md:w-[500px]  lg:w-[600px]   px-2">
+        <div className=" w-full md:w-[500px]  lg:w-[600px]   px-3">
           <p className="text-[12px] md:text-[18px] leading-[1.6em] mt-3 font-light tracking-[.02em]  text-justify">
             {selectedImg.comment ? selectedImg.comment : ""}
           </p>
